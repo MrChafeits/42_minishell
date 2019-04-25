08a829f4b183951f3000b58cb300ffbceea66393
Clayton Allen <callen@student.42.us.org>
2019-04-25 02:52:21 -0700

    investigating hash table implementations

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: callen <callen@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/12 14:56:58 by callen            #+#    #+#             */
-/*   Updated: 2019/04/22 20:12:25 by callen           ###   ########.fr       */
+/*   Updated: 2019/04/23 00:23:19 by callen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,6 +95,56 @@ void		free_strtab(char ***t)
 		ft_memdel((void**)&(*t));
 	}
 }
+//TODO: decide whether or not this is a bad idea
+typedef struct s_bkt	t_bkt;
+struct	s_bkt
+{
+	t_bkt		*next;
+	char		*key;
+	void		*data;
+	unsigned	khash;
+	int			nfnd;
+};
+typedef struct s_htab	t_htab;
+struct	s_htab
+{
+	t_bkt	**bkt_arr;
+	int		nbkts;
+	int		nentr;
+};
+typedef int	t_hfun(t_bkt*);
+extern t_htab	*hash_create(int size);
+extern t_htab	*hash_copy(t_htab *tbl, void (*f)());
+extern void		hash_flush(t_htab *tbl, void (*f)());
+extern void		hash_delet(t_htab *tbl);
+extern void		hash_walk(t_htab *tbl, t_hfun (*f)());
+extern int		hash_bkt(const char *s, t_htab *tbl);
+extern int		hash_size(t_htab *tbl);
+extern t_bkt	*hash_search(const char *s, t_htab *tbl, int l);
+extern t_bkt	*hash_insert(char *s, t_htab *tbl, int l);
+extern t_bkt	*hash_remove(const char *s, t_htab *tbl, int l);
+extern unsigned	hash_string(const char *s);
+#define hash_items(bucket, table) ((table && (bucket < table->nbkts)) ? table->bkt_arr[bucket] : (t_bkt*)NULL)
+#define HTAB_DEF 128
+#define HTAB_ENT(ht) ((ht) ? (ht)->nentr : 0)
+#define HTAB_NOSRCH 0x01
+#define HTAB_CREATE 0x02
+
+typedef struct s_bc	t_bc;
+struct	s_bc
+{
+	char	*cmd;
+	void	(*f)(t_margs*);
+};
+
+static t_bc	g_cmds[] = {
+	{"cd", &cd_builtin},
+	{"echo", &echo_builtin_cmd},
+	{"env", &env_builtin},
+	{"exit", &exit_builtin},
+	{"setenv", &setenv_builtin},
+	{"unsetenv", &unsetenv_builtin},
+};
 
 int			msh_exec(char *ln, t_margs *m)
 {
